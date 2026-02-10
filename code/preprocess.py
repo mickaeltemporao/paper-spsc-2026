73041e721696d09a5d0d2515a202e006e269f016
Mickael Temporão <16172142+mickaeltemporao@users.noreply.github.com>
2026-02-11 00:59:57 +0100

    preprocess script ready

--- code/preprocess.py
+++ code/preprocess.py
@@ -9,20 +9,24 @@ def subset():
 
         vars_dict = {
             "cntry": "country",
-            "agea": "age",  
+            # "agea": "age",  
             "clsprty": "closer_party_dummy",  # Feel closer to a particular party than all other parties
-            "edlvdfr": "education",
-            "gndr": "gender",
+            # "edlvdfr": "education",
+            # "gndr": "gender",
             "lrscale": "ideology",  # Left-right self-placement
-            "marsts": "mrt_statuts",
             "polintr": "pol_int",
             "prtclffr": "closer_party",  # Which party feel closer to, France
             "prtdgcl": "closer_party_likert", 
             "prtvtefr": "previous_vote",
             "vote": "vote",
+            "trstplt": "trust_politicians",
+            "trstprt": "trust_parties",
+            "stfgov": "satisfaction_gov", 
+            "stfdem": "satisfaction_dem",
         }
 
         df = df_raw[vars_dict.keys()]
-        df = df.rename(columns=vars_dict)  # The advantage is that the order doesn't matter. It matches the keys!
+        # df = df.rename(columns=dict(vars_dict))
         df.to_csv("data/ess_subset.csv", index=False)
         return pd.read_csv("data/ess_subset.csv") 
+
